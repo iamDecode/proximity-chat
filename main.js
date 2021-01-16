2a5b9451fa1c166eea09c12cab6206fac310f7e4
Dennis Collaris <d.collaris@me.com>
2021-01-16 22:21:45 +0100

    fix: remove unnecessary limitations in fps and resolution

--- main.js
+++ main.js
@@ -44,27 +44,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-const throttle = (func, limit) => {
-  let lastFunc
-  let lastRan
-  return function() {
-    const context = this
-    const args = arguments
-    if (!lastRan) {
-      func.apply(context, args)
-      lastRan = Date.now()
-    } else {
-      clearTimeout(lastFunc)
-      lastFunc = setTimeout(function() {
-        if ((Date.now() - lastRan) >= limit) {
-          func.apply(context, args)
-          lastRan = Date.now()
-        }
-      }, limit - (Date.now() - lastRan))
-    }
-  }
-}
-
 // track which users are connected
 const users = {};
 
@@ -112,9 +91,9 @@ const usocket = uws.SSLApp({key_file_name: keyFile, cert_file_name: certFile}).w
       }));
 
       const user = { ws, id, name, audioEnabled: true, videoEnabled: true, pos, broadcast: false };
-      user.emitPos = throttle((x, y) => {
+      user.emitPos = (x, y) => {
         usocket.publish('position', String([id, x, y]));
-      }, 25);
+      }
 
       users[id] = user;
       return
