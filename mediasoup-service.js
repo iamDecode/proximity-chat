b5eeada2271a231d74efdc13e981244c53dad0d0
Dennis Collaris <d.collaris@me.com>
2021-02-06 13:25:20 +0100

    feat: enable setting announced_ip config with env var

--- mediasoup-service.js
+++ mediasoup-service.js
@@ -50,7 +50,7 @@ const config = {
     listenIps: [
       {
         ip: '0.0.0.0',
-        announcedIp: '127.0.0.1', // replace by public IP address
+        announcedIp: process.env.ANNOUNCED_IP || '127.0.0.1', // replace by public IP address
       },
     ],
     maxIncomingBitrate: 1500000,
