9d2822ceee89055c159a207f10f990cc8e652b5d
Dennis Collaris <d.collaris@me.com>
2021-03-06 14:59:46 +0100

    refactor: move application state into App module

---
+++ public/app.js
@@ -0,0 +1,229 @@
+import {MediasoupClient} from './mediasoup-client.js';
+import {Player, SelfPlayer} from './player.js';
+import {Socket} from './socket.js';
+import {attachSinkId} from './utils.js';
+
+
+// Config
+const SOUND_CUTOFF_RANGE = 350;
+const SOUND_NEAR_RANGE = 200;
+
+
+export class App {
+  constructor(socket) {
+    this.players = new Map();
+    this.selfPlayer = null;
+
+    this.socket = new Socket(`wss://${location.hostname}:9001`);
+    this.socket.onmessage = async (...args) => await this.handleMessage(...args);
+    this.socket.onopen = (_) => {
+      this.mediasoupClient = new MediasoupClient(this.socket);
+      this.mediasoupClient.init().then((_) => {
+        navigator.mediaDevices.enumerateDevices().then(window.gotDevices);
+        this.socket.send(['connect', localStorage.getItem('name')]);
+      });
+    };
+
+    this.playerDelegate = this.getPlayerDelegate();
+
+    // Start render loop for audioRing visualizations (10 fps)
+    const performAnimation = () => {
+      if (this.selfPlayer != null) this.selfPlayer.render();
+      for (const player of this.players.values()) player.render();
+    };
+
+    setInterval(performAnimation, 1000/10);
+  }
+
+  async handleMessage(message) {
+    /* eslint-disable brace-style
+       --
+       The if/else if below read nicer with a blank line between them. */
+    let data;
+    if (message.data[0] == '{') {
+      data = JSON.parse(message.data);
+    } else if (message.data == 'pong') {
+      return;
+    } else {
+      data = {position: message.data.split(',')};
+    }
+
+    // Setup peer when user receives id
+    if ('id' in data) {
+      if (this.selfPlayer != null) {
+        console.log('destroying old identity', this.selfPlayer.id, 'and replacing with', data.id);
+        peer.destroy();
+        peer = undefined;
+        return;
+      }
+
+      const name = localStorage.getItem('name');
+      const stream = this.mediasoupClient.stream;
+
+      this.selfPlayer = new SelfPlayer(data.id, name, data.pos, this.playerDelegate);
+      this.selfPlayer.stream = stream;
+      this.selfPlayer.audioEnabled = stream.getAudioTracks()[0] != null;
+      this.selfPlayer.videoEnabled = stream.getVideoTracks()[0] != null;
+      await this.playStream(stream, this.selfPlayer);
+
+      setInterval((_) => {
+        this.socket.send('ping');
+      }, 10000);
+    }
+
+    // Populate existing players
+    else if ('players' in data) {
+      for (const p of Object.values(data.players)) {
+        const player = new Player(
+            p.id,
+            p.name,
+            {x: parseInt(p.pos.x), y: parseInt(p.pos.y)},
+            this.playerDelegate,
+        );
+
+        player.audioEnabled = p.audioEnabled;
+        player.videoEnabled = p.videoEnabled;
+        player.setBroadcast(p.broadcast);
+
+        this.players.set(p.id, player);
+
+        this.startCall(p.id);
+      }
+    }
+
+    // Talk to any user who joins
+    else if ('join' in data) {
+      if (data.join.id == this.selfPlayer.id) {
+        return;
+      }
+
+      console.log('calling', data.join.id);
+      const player = new Player(data.join.id, data.join.name, data.join.pos, this.playerDelegate);
+
+      player.audioEnabled = true;
+      player.videoEnabled = true;
+
+      this.players.set(data.join.id, player);
+
+      this.startCall(data.join.id);
+    }
+
+    // Update player position
+    else if ('position' in data) {
+      if (this.players.has(data.position[0])) {
+        const player = this.players.get(data.position[0]);
+        player.setPosition(parseInt(data.position[1]), parseInt(data.position[2]));
+      }
+    }
+
+    // Update player properties
+    else if ('update' in data) {
+      if (this.players.has(data.update.id)) {
+        const player = this.players.get(data.update.id);
+        player.name = data.update.name;
+        player.audioEnabled = data.update.audioEnabled;
+        player.videoEnabled = data.update.videoEnabled;
+        player.setBroadcast(data.update.broadcast);
+      }
+    }
+
+    // Remove players who left or disconnected
+    else if ('leave' in data) {
+      console.log('call dropped from', data.leave.id);
+      // Remove player from players list
+
+      if (this.players.has(data.leave.id)) {
+        const player = this.players.get(data.leave.id);
+        player.tooltip.tooltip('dispose');
+        player.$elem.remove();
+        this.players.delete(player.id);
+      };
+    }
+  }
+
+  async playStream(stream, target) {
+    // Create the video element for the stream
+    const $video = document.createElement('video');
+    $video.srcObject = stream;
+    $video.autoplay = true;
+    $video.playsInline = true;
+
+    if (window.sinkId != null) {
+      attachSinkId($video, window.sinkId);
+    }
+
+    // Add it to the player
+    if (target instanceof SelfPlayer) {
+      $video.muted = true;
+      $video.setAttribute('data-peer', target.id);
+      target.addVideo($video);
+    } else {
+      $video.setAttribute('data-peer', target);
+      const player = this.players.get(target);
+      player.addVideo($video);
+    }
+
+    try {
+      await elem.play();
+    } catch (e) {
+    }
+  }
+
+  async startCall(target) {
+    console.log('starting call with ', target);
+    const player = this.players.get(target);
+
+    if (player == null) {
+      console.log('couldn\'t find player for stream', call.id);
+    } else if (player.stream == null) {
+      player.stream = await this.mediasoupClient.createStream(target);
+      await this.playStream(player.stream, target);
+      // To ensure volume relative to position is set correctly.
+      player.setPosition(player.x, player.y);
+      console.log('created stream for', target);
+    }
+  }
+
+  getPlayerDelegate() {
+    return {
+      calcVolume: (player) => {
+        if (player.broadcast) {
+          return 1;
+        }
+
+        // calulate angle and distance from listener to sound
+        const dist = Math.hypot(player.y - this.selfPlayer.y, player.x - this.selfPlayer.x);
+        const scale = 1 - (dist - SOUND_NEAR_RANGE) / (SOUND_CUTOFF_RANGE - SOUND_NEAR_RANGE);
+
+        // target is too far away, no volume
+        if (dist > SOUND_CUTOFF_RANGE) {
+          return 0;
+        }
+
+        // target is very close, max volume
+        if (dist < SOUND_NEAR_RANGE) {
+          return 1;
+        }
+
+        return scale;
+      },
+      pause: (id) => {
+        this.socket.send(['pause', null, id]);
+      },
+      resume: (id) => {
+        this.socket.send(['resume', null, id]);
+      },
+      position: (x, y) => {
+        this.socket.send(['pos', x, y]);
+      },
+      update: (name, audio, video, broadcast) => {
+        this.socket.send(['update', name, audio, video, broadcast]);
+      },
+      updatePlayers: () => {
+        for (const player of this.players.values()) {
+          player.setPosition(player.x, player.y);
+        }
+      },
+    };
+  };
+}
