707f24931d075e49922408019d493cd594863eb8
Dennis Collaris <d.collaris@me.com>
2021-03-29 22:26:00 +0200

    feat: support multiple rooms by duplicating services

--- public/room/config.js
+++ public/room/config.js
@@ -1,24 +1,39 @@
 (function(exports) {
   exports.ROOM_CONFIG = {
-    // The size of the room background image. These must be the dimensions of
-    // room.png.
-    width: 3200,
-    height: 1800,
+    'Room 1': {
+      // The size of the room background image. These must be the dimensions of
+      // room.png.
+      width: 3200,
+      height: 1800,
+      background: 'room.png',
 
-    // The position at which new players enter the map. They may be offset by a
-    // small amount from this location so they don't overlap each other.
-    starting_position: {
-      x: 100,
-      y: 100,
+      // The position at which new players enter the map. They may be offset by a
+      // small amount from this location so they don't overlap each other.
+      starting_position: {
+        x: 100,
+        y: 100,
+      },
+
+      // Optional key to show a menu at the specified location where users can
+      // obtain a drink icon.
+      drinks: {
+        x: 1785,
+        y: 280,
+        range: 400,
+        duration: 1000 * 60 * 10, // in ms
+      },
     },
-
-    // Optional key to show a menu at the specified location where users can
-    // obtain a drink icon.
-    drinks: {
-      x: 1785,
-      y: 280,
-      range: 400,
-      duration: 1000 * 60 * 10, // in ms
+    'Room 2': {
+      width: 3200,
+      height: 1800,
+      background: 'room.png',
+      starting_position: {x: 100, y: 100},
+      drinks: {
+        x: 1785,
+        y: 280,
+        range: 400,
+        duration: 1000 * 60 * 10,
+      },
     },
   };
 })(typeof exports === 'undefined'? document: exports);
